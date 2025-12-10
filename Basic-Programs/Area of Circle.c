ab97e3bd4d17e942e8212d08f6bc1f0501e6aa26
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-10 15:52:47 +0530

    AOC

---
+++ Basic-Programs/Area of Circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    float area, radius;
+    float pi = 3.14;
+
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+
+    area = pi * radius * radius;
+
+    printf("Area of Circle: %.2f\n", area);
+    return 0;
+}
