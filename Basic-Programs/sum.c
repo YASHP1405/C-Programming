d5d4357d47df72d5c1970d26736d5be1c235eac5
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-13 23:41:41 +0530

    REvision

---
+++ Basic-Programs/sum.c
@@ -0,0 +1,11 @@
+/* 3. Addition of Two Numbers – Take two integers as input and print their sum. */
+#include <stdio.h>
+int main() 
+{
+    int a,b,sum;
+    printf("Enter two integers: ");
+    scanf("%d %d",&a,&b);
+    sum = a + b;
+    printf("Sum: %d\n",sum);
+    return 0;
+}
