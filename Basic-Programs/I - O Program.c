ba2ccd4bac18b252642890314577da42117cfe60
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-13 23:45:06 +0530

    Revision

---
+++ Basic-Programs/I - O Program.c
@@ -0,0 +1,9 @@
+/* 2. Simple Input & Output – Accept an integer and print it. */
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter an integer: ");
+    scanf("%d", &a);
+    printf("You entered: %d\n", a);
+    return 0;
+}
