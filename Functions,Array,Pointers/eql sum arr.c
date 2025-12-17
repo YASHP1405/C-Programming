1a4120bffde82b5cb70b57aaacb656d97aaff8e5
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-17 23:34:30 +0530

    Practise

---
+++ Functions,Array,Pointers/eql sum arr.c
@@ -0,0 +1,30 @@
+/* 25. Find pair of elements whose sum is equal to a given number */
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, sum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the required sum: ");
+    scanf("%d", &sum);
+
+    printf("Pairs with sum %d are:\n", sum);
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == sum) {
+                printf("(%d, %d)\n", arr[i], arr[j]);
+            }
+        }
+    }
+
+    return 0;
+}
