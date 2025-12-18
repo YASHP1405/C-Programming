f10d8089ef8a33655216cb42733574768abcd42c
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-18 23:35:04 +0530

    Practise

--- Functions,Array,Pointers/2D array/2d array.c
+++ Functions,Array,Pointers/2D array/2d array.c
@@ -1,11 +1,25 @@
-#include<stdio.h>
-int main(){
-    int a[5][5];
-    int i,j;
-    for (i=0;i<5;i++){
-        for (j=0;j<5;j++){
-            scanf("%d",&a[i][j]);
+/* 2D array example */
+#include <stdio.h>
+
+int main() {
+    int a[3][2];
+    int i, j;
+
+    printf("Enter elements of 3x2 matrix:\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
+
+    printf("\nMatrix is:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
---
+++ Functions,Array,Pointers/2D array/2d mat adding.c
@@ -0,0 +1,39 @@
+/* Adding matrices */
+#include <stdio.h>
+
+int main() {
+    int a[3][2], b[3][2], c[3][2];
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Enter elements of matrix A:\n");
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Enter elements of matrix B:\n");
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nResultant matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
