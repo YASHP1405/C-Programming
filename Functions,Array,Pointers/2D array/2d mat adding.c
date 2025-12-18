73740fdb63414d52735e5359733c194325baae57
Yash Uttam Pawar <yupawar689@gmail.com>
2025-12-18 23:41:22 +0530

    Practise

--- Functions,Array,Pointers/2D array/2d mat adding.c
+++ Functions,Array,Pointers/2D array/2d mat adding.c
@@ -24,10 +24,10 @@ int main() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
             c[i][j] = a[i][j] + b[i][j];
+            printf("\nResultant matrix:\n");
         }
     }
 
-    printf("\nResultant matrix:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
             printf("%d ", c[i][j]);
