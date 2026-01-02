f1f7aa3d762a5759f12e05563ff49159e03d845c
Yash Uttam Pawar <yupawar689@gmail.com>
2026-01-02 23:48:14 +0530

    Array revision

--- Functions,Array,Pointers/Array/without modify.c
+++ Functions,Array,Pointers/Array/without modify.c
@@ -5,13 +5,11 @@
 int main() {
     int a[5], i;
 
-    // Input array
     for(i = 0; i < 5; i++) {
         printf("Enter element %d: ", i + 1);
         scanf("%d", &a[i]);
     }
 
-    // Print array in reverse (without modifying)
     printf("Array in reverse order:\n");
     for(i = 4; i >= 0; i--) {
         printf("%d ", a[i]);
